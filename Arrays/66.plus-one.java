9780307e82a598263d5048509c531dce266db0db
“thamothara7” <thamothara2017@gmail.com>
2025-11-07 22:33:14 +0530

    PlusOne

--- Arrays/66.plus-one.java
+++ Arrays/66.plus-one.java
@@ -7,6 +7,18 @@
 // @lc code=start
 class Solution {
     public int[] plusOne(int[] digits) {
+        for(int i = digits.length-1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            else {
+                digits[i] = 0;
+            }
+        }
+        int[] arr = new int[digits.length+1];
+        arr[0] = 1;
+        return arr;
         
     }
 }
--- Arrays/arrBasic.java
+++ Arrays/arrBasic.java
@@ -1,13 +1,17 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class arrBasic {
 
     public static void main(String[] args) {
-        int[] zig = new int[10];
-        int[] zag = zig;
-        zig[0] = 1;
-        System.out.println(Arrays.toString(zag));
-        System.out.println(zag[0]);
-
+        // int[] zig = new int[10];
+        // int[] zag = zig;
+        // zig[0] = 1;
+        // System.out.println(Arrays.toString(zag));
+        // System.out.println(zag[0]);
+        int[] arr = {1,2,3,4} ;       
+        
+            System.out.println(Arrays.toString(arr));
+        
     }
 }
