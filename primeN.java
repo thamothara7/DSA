2a85b299f06b9c3417b65769730a3913bba12286
“thamothara7” <thamothara2017@gmail.com>
2025-12-05 15:44:19 +0530

    PrimeNumber

--- primeN.java
+++ primeN.java
@@ -1,25 +1,30 @@
 import java.util.*;
+
 public class primeN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of N:");
         int N = sc.nextInt();
         int c = 2;
-        if(N <= 1){
+        boolean isPrime = true;
+        if (N <= 1) {
             System.out.println(N + " Neither prime nor composite");
-        }
-        while (c*c <= N) {
-            if (N%c == 0) {
-                System.out.println(N + " is not a prime number");
-                break;
-            } else {
-                c++;
-            }
-           
-        }
-        System.out.println(N + " is a prime number");
-        
+        } else {
+            while (c * c <= N) {
+                if (N % c == 0) {
+                    isPrime = false;
+                    break;
+                } else {
+                    c++;
+                }
 
+            }
+        }
+        if (isPrime) {
+            System.out.println(N + " is a prime number");
+        } else {
+            System.out.println(N + " is not a prime number");
+        }
 
     }
 }
